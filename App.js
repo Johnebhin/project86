2f5efbc9a4daad78c41a20ceade54f0d25dc0c7c
Johnebhin <121502677+Johnebhin@users.noreply.github.com>
2023-10-17 20:00:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import HomeScreen from './screens/Home';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <HomeScreen/>
+            
+        )
+    }
+}
